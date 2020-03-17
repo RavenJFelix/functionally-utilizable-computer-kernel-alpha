ed97e317a9db08227bc942d1fa2cdfe77264b55e
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-17 05:04:22 -0500

    Address practice

--- boot.asm
+++ boot.asm
@@ -1,14 +1,14 @@
+;assembly stuff
 mov ah, 0x0e
-mov al, 'F'
-int 0x10
-mov al, 'U'
-int 0x10
-mov al, 'C'
-int 0x10
-mov al, 'K'
+
+mov bx, fuck
+add bx, 0x7c00
+mov al, [bx]
 int 0x10
 
-jmp $ ;asdf
+jmp $
 
+fuck:
+	db 'X'
 times 510 - ($-$$) db 0
 dw 0xaa55
