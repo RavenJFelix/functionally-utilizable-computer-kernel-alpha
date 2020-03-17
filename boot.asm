31071914a9862e7d98f9529b0114a885c14e5169
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-17 08:28:35 -0500

    Comparison translations

--- boot.asm
+++ boot.asm
@@ -1,13 +1,22 @@
-mov ah, 0x0e
-mov bx, 4
-cmp bx, 4
-je then_block
-mov al, "!"
-jmp then_else
+mov bx, 40
 
-then_block:
-mov al, "="
-then_else:
+cmp bx, 4
+jle if_true
+cmp bx, 40
+jl else_if_1
+mov al, 'C'
+jmp if_end
+
+
+if_true:
+mov al, 'A'
+jmp if_end
+else_if_1:
+mov al, 'B'
+jmp if_end
+
+if_end:
+mov ah, 0x0e
 int 0x10
 
 jmp $
