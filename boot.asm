3be5955fd7fb8cad3ef961eb1dd28573518d0a18
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-17 07:49:53 -0500

    Learning about control structures

--- boot.asm
+++ boot.asm
@@ -1,24 +1,16 @@
 mov ah, 0x0e
+mov bx, 4
+cmp bx, 4
+je then_block
+mov al, "!"
+jmp then_else
 
-mov bp, 0x8000
-mov sp, bp
-
-push 'A'
-push 'B'
-push 'C'
-
-pop bx
-mov al, bl
-int 0x10
-
-pop bx
-mov al, bl
-int 0x10
-
-mov al, [0x7ffe]
+then_block:
+mov al, "="
+then_else:
 int 0x10
 
 jmp $
 
-times 510-($-$$) db 0
+times 510 - ($-$$) db 0
 dw 0xaa55
