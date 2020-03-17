f316dc92d7da734f473240bee637f90eed1936a8
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-17 10:46:23 -0500

    Improve indentation

--- print_string.asm
+++ print_string.asm
@@ -1,13 +1,13 @@
 print_string:
-pusha
-mov ah, 0x0e ; int 10/ah = 0eh ->scrolling teletype BIOS routine
+	pusha
+	mov ah, 0x0e ; int 10/ah = 0eh ->scrolling teletype BIOS routine
 .loop:
-cmp byte [bx], 0 ;Check if the character is null terminated
-je exit
-mov al, [bx] ; If not, move the character to al
-int 0x10 ; Print chraracter
-add byte bx, 1
-jmp .loop
+	cmp byte [bx], 0 ;Check if the character is null terminated
+	je exit
+	mov al, [bx] ; If not, move the character to al
+	int 0x10 ; Print chraracter
+	add byte bx, 1
+	jmp .loop
 exit:
-popa
-ret
+	popa
+	ret
