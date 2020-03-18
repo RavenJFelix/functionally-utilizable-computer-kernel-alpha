4e63cb1b3ecb4570e8fda8aa8b7da29e27602fcc
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-17 20:41:24 -0500

    Save

--- boot.asm
+++ boot.asm
@@ -1,29 +1,56 @@
+[org 0x7c00]
+  mov [BOOT_DRIVE], dl
+
+  mov bp, 0x8000
+  mov sp, bp    
+  
+  mov bx, 0xa000
+  mov es, bx
+  mov bx, 0x9000
+  mov dh, 2
+
+  call disk_load
+
+  mov dx, [0x9000]
+  call print_hex
+
+  mov dx, [0x9000 + 512]
+  call print_hex
+
+  jmp $
+
 %include "print_string.asm"
-mov ah, 0x02 ;BIOS 0x13 read sector function
+%include "print_hex.asm"
 
-mov dl, 0 ;First drive
-mov ch, 3 ; Cylinder 3
-mov dh, 1 ; 4th Sector
+ disk_load:
+  push dx;push to stack so it can be recalled
+  
+  mov ah, 0x02 ; Bios read sctor function
+  mov al, dh   ; Jump of sectors to read
+  mov ch, 0x00 ; Cylinder 0
+  mov dh, 0x00 ; Head 0
+  mov cl, 2  ; Second sector
 
-mov cl, 4 ;4th Sector
+  int 0x13     ; BIOS interrupt
 
-mov al, 5 ;Read 5 sectors
-mov bx, 0xa000
-mov es, bx
-mov bx, 0x1234
-
-int 0x13
-
-jc disk_error
-jmp after_error
+  jc disk_error ;Check for carry flag
+  
+  pop dx
+  ;cmp dh, al ; See if the number of sectors read are equal
+  ;jne disk_error
+  ret
 
 disk_error:
-mov	bx, DISK_ERROR_MSG
-call print_string
-after_error:
-jmp $
+  mov bx, DISK_ERROR_MSG
+  call print_string
+  jmp $
 
-DISK_ERROR_MSG: db "Disk error fuck", 0
+ 
+DISK_ERROR_MSG: db "FUCK THE DISK READ BROKE", 0
+
+BOOT_DRIVE: db 0
 
 times 510-($-$$) db 0
 dw 0xaa55
+times 256 dw 0xdada
+times 256 dw 0xface
--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 nasm -f bin boot.asm -o os.bin
-qemu-system-x86_64 os.bin
+qemu-system-x86_64 -drive format=raw,file=os.bin
