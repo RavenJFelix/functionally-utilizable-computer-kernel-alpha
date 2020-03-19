9d175a6fc22cd673c5a6fcde41061b11be2c52c2
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-19 13:40:29 -0500

    Add basic screen char driver

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-OBJ = loader.o kmain.o
+OBJ = loader.o kmain.o screen.o
 CC = gcc
 CFLAGS = -m32 -nostdlib -nostdinc -fno-builtin -fno-stack-protector \
 		 -nostartfiles -nodefaultlibs -Wall -Wextra -Werror -c
--- kmain.c
+++ kmain.c
@@ -1,9 +1,15 @@
+#include "screen.h"
 void kmain()
 {
+	char *frame_buffer = (char*)0x000b8002 + 160;
+
+	frame_buffer[0] = 'F';
+	frame_buffer[2] = 'U';
 	//Have the caharcter point to the first text cell
 	//of the video memory
-	char* video_memory = (char*) 0x000b8000;
 	// Store the character F at the top left 
-	*video_memory = 'F';
-	video_memory[2] = 'U';
+	struct uc_pair2d pos = {1,2};
+	
+	fb_write_char(0, 0, 'F', 0, 15);
+	fb_write_char_pos(pos, 'U', 0, 15);
 }
