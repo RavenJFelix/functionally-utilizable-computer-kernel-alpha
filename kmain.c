a26d3fea82292947c51ef97be15c5e57aa462cfe
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-18 23:53:39 -0500

    Add VGA test

--- kmain.c
+++ kmain.c
@@ -2,7 +2,8 @@ void kmain()
 {
 	//Have the caharcter point to the first text cell
 	//of the video memory
-	char* video_memory = (char*) 0xb8000;
+	char* video_memory = (char*) 0x000b8000;
 	// Store the character F at the top left 
 	*video_memory = 'F';
+	video_memory[2] = 'U';
 }
