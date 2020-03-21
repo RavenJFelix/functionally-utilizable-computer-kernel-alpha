3ad78b7f284056ddbf9e1fef2059ccd9f32bc195
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-21 01:10:03 -0500

    Make the test kernel work

--- boot/loader.s
+++ boot/loader.s
@@ -2,3 +2,4 @@
 ;mov word [0xb8000], 0x1f41
 extern main
 call main
+hlt
--- boot/testkernel.c
+++ boot/testkernel.c
@@ -1,5 +1,5 @@
 void main()
 {
-	char* video_mem =(char*) 0xb8000;
-	*video_mem = 'F';
+	char* video_mem = (char*) 0xb8000;
+	*video_mem = 'Z';
 }
