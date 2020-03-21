25955ae155bc39bac7f91bebd4e468d0dfa003c1
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-21 14:55:16 -0500

    Sort screen constants alphabetically

--- include/screen.h
+++ include/screen.h
@@ -15,22 +15,22 @@ struct fb_color_code
 	unsigned char bg;
 };
 
-void fb_write_string_wrap_auto(struct uc_pair2d pos, const char *str, const struct fb_color_code color_code);
-void fb_write_string_wrap_safe(struct uc_pair2d pos, const char *str, const struct fb_color_code color_code);
-struct uc_pair2d fb_char_line_pos_to_pos(unsigned int line_pos);
-void fb_write_char_simp(unsigned int line_pos, char c);
 bool line_pos_exceeds_max(unsigned int line_pos);
-void fb_write_string_wrap_direct(struct uc_pair2d pos, const char *str, const struct fb_color_code color_code);
 struct uc_pair2d fb_char_line_pos_to_pos(unsigned int line_pos);
-void fb_write_char(unsigned int line_pos, char c, unsigned char color_code);
-void fb_write_char_fast(unsigned char x, unsigned char y, char c, unsigned char fg, unsigned char bg);
-void fb_write_char_abstract(struct uc_pair2d pos, char c, struct fb_color_code color_code);
-void fb_moveCursor(struct uc_pair2d pos);
-void fb_write_string_direct_noNull(struct uc_pair2d pos, const char* string, unsigned int len, struct fb_color_code color_code);
-void fb_write_string_direct(struct uc_pair2d pos, const char* str, const struct fb_color_code color_code);
-void fb_shift_up(unsigned int distance);
-void fb_shift_down(unsigned int distance);
+struct uc_pair2d fb_char_line_pos_to_pos(unsigned int line_pos);
 void fb_clear_line(unsigned char line);
+void fb_moveCursor(struct uc_pair2d pos);
+void fb_shift_down(unsigned int distance);
+void fb_shift_up(unsigned int distance);
+void fb_write_char(unsigned int line_pos, char c, unsigned char color_code);
+void fb_write_char_abstract(struct uc_pair2d pos, char c, struct fb_color_code color_code);
+void fb_write_char_fast(unsigned char x, unsigned char y, char c, unsigned char fg, unsigned char bg);
+void fb_write_char_simp(unsigned int line_pos, char c);
+void fb_write_string_direct(struct uc_pair2d pos, const char* str, const struct fb_color_code color_code);
+void fb_write_string_direct_noNull(struct uc_pair2d pos, const char* string, unsigned int len, struct fb_color_code color_code);
+void fb_write_string_wrap_auto(struct uc_pair2d pos, const char *str, const struct fb_color_code color_code);
+void fb_write_string_wrap_direct(struct uc_pair2d pos, const char *str, const struct fb_color_code color_code);
+void fb_write_string_wrap_safe(struct uc_pair2d pos, const char *str, const struct fb_color_code color_code);
 
 void fb_write_string_direct(struct uc_pair2d pos, const char* str, const struct fb_color_code color_code);
 #endif
