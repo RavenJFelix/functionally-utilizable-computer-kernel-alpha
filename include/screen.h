c0d45c45cac0be928345a6157597774e7e11cfae
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-21 10:43:07 -0500

    Correct max line pos constant

--- include/screen.h
+++ include/screen.h
@@ -27,6 +27,8 @@
 #define FB_LINE_POS(x, y) 2 * (FB_MAX_COLS * y + x)
 #define FB_CURSOR_LINE_POS(x, y) (FB_MAX_COLS * y + x)
 #define FB_COLOR_CODE_STRUCT_TO_UCHAR(fg, bg) ((fg & 0x0f) << 4) | (bg & 0x0f) //Takes two unsigned chars
+
+#define FB_MAX_LINE_POS (FB_LINE_POS(24, 79))
 struct fb_color_code
 {
 	unsigned char fg;
@@ -40,9 +42,12 @@ struct fb_state
 };
 
 
+void fb_write_string_wrap_auto(struct uc_pair2d pos, const char *str, const struct fb_color_code color_code);
+void fb_write_string_wrap_safe(struct uc_pair2d pos, const char *str, const struct fb_color_code color_code);
+struct uc_pair2d fb_char_line_pos_to_pos(unsigned int line_pos);
 void fb_write_char_simp(unsigned int line_pos, char c);
 bool line_pos_exceeds_max(unsigned int line_pos);
-void fb_write_string_wrap(struct uc_pair2d pos, const char *str, const struct fb_color_code color_code);
+void fb_write_string_wrap_direct(struct uc_pair2d pos, const char *str, const struct fb_color_code color_code);
 struct uc_pair2d fb_char_line_pos_to_pos(unsigned int line_pos);
 void fb_write_char(unsigned int line_pos, char c, unsigned char color_code);
 void fb_write_char_fast(unsigned char x, unsigned char y, char c, unsigned char fg, unsigned char bg);
