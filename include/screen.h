92db7f9d485ee27b1e286dec2fea4556e498039e
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-21 10:07:51 -0500

    Move FB color codes to top

--- include/screen.h
+++ include/screen.h
@@ -2,6 +2,23 @@
 #define SCREEN_H
 #include "bool.h"
 #include "pair2d.h"
+#define FB_BLACK  0;
+#define FB_BLUE = 1;
+#define FB_GREEN = 2;
+#define FB_CYAN = 3;
+#define FB_RED = 4;
+#define FB_MAGENTA = 5;
+#define FB_BROWN = 6;
+#define FB_LIGHT_GREY = 7;
+#define FB_DARK_GREY = 8;
+#define FB_LIGHT_BLUE = 9;
+#define FB_LIGHT_GREEN = 10;
+#define FB_LIGHT_CYAN = 11;
+#define FB_LIGHT_RED = 12;
+#define FB_LIGHT_MAGENTA = 13;
+#define FB_LIGHT_BROWN = 14;
+#define FB_WHITE = 15;
+
 #define FB_MAX_ROWS 25
 #define FB_MAX_COLS 80
 
@@ -38,20 +55,4 @@ void fb_shift_down(unsigned int distance);
 void fb_clear_line(unsigned char line);
 
 void fb_write_string_direct(struct uc_pair2d pos, const char* str, const struct fb_color_code color_code);
-#define FB_BLACK  0;
-#define FB_BLUE = 1;
-#define FB_GREEN = 2;
-#define FB_CYAN = 3;
-#define FB_RED = 4;
-#define FB_MAGENTA = 5;
-#define FB_BROWN = 6;
-#define FB_LIGHT_GREY = 7;
-#define FB_DARK_GREY = 8;
-#define FB_LIGHT_BLUE = 9;
-#define FB_LIGHT_GREEN = 10;
-#define FB_LIGHT_CYAN = 11;
-#define FB_LIGHT_RED = 12;
-#define FB_LIGHT_MAGENTA = 13;
-#define FB_LIGHT_BROWN = 14;
-#define FB_WHITE = 15;
 #endif
