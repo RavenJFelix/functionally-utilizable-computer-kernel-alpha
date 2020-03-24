e31cdd3baf37d51bae6b7ad9506f6b52348282ec
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-23 20:59:32 -0500

    Add struct stuff

--- drivers/VGA/vga_util.c
+++ drivers/VGA/vga_util.c
@@ -8,9 +8,9 @@ bool fb_line_pos_exceeds_max(unsigned int line_pos)
 	return (line_pos > FB_MAX_LINE_POS);
 }
 
-struct uc_pair2d fb_char_line_pos_to_pair2d(unsigned int line_pos)
+uc_pair2d fb_char_line_pos_to_pair2d(unsigned int line_pos)
 {
 	unsigned char x = (char) (((line_pos/2) % FB_MAX_COLS)) ;
 	unsigned char y = (char) ( ( ((line_pos/2) / FB_MAX_COLS)));
-	return (struct uc_pair2d){x,y};
+	return (uc_pair2d){x,y};
 }
