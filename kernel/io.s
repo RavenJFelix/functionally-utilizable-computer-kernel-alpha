0a55f84f75d61ef75ffb975ec458e8270d942838
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-24 14:06:10 -0500

    have read byte function

--- kernel/io.s
+++ kernel/io.s
@@ -7,3 +7,10 @@ mov al, [esp + 8] ;
 mov dx, [esp + 4] ; Address of port to dx
 out dx, al        ; Send data in al to dx
 ret               ; return to caller
+
+global inb
+; in - reads byte from I/O port
+inb:
+; stack [esp + 4] ; The port to be read from
+in [esp + 4], al
+ret
