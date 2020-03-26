3ccbf55cdccd7ce668d6e25ca57e6c58b81e0f30
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-25 23:38:00 -0500

    Add keyboard kernel variable initialization

--- kernel/kernel_globals.c
+++ kernel/kernel_globals.c
@@ -1,3 +1,4 @@
+#include "keyboard.h"
 #include "kernel_globals.h"
 #include "terminal.h"
 
@@ -5,6 +6,9 @@
 #include "vga_const_color.h"
 Terminal main_terminal = {{0,0}, {FB_MAX_COLS, FB_MAX_ROWS}, FB_MATRIX_COLORZ };
 uc_ring_buffer kernel_keyboard_buffer;
+
+Keyboard kernel_keyboard;
+
 
 unsigned char kernel_keyboard_buffer_data[KERNEL_KEYBOARD_BUFFER_SIZE];
 void kernel_globals_init()
@@ -14,4 +18,6 @@ void kernel_globals_init()
 	kernel_keyboard_buffer.back = 0;
 	kernel_keyboard_buffer.data = kernel_keyboard_buffer_data;
 	kernel_keyboard_buffer.length = KERNEL_KEYBOARD_BUFFER_SIZE;
+
+	kernel_keyboard.caps = false;
 }
