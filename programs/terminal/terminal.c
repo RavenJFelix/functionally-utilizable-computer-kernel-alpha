0da2b9ecd1401b48a97a44cd66b12d9e150c0224
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-25 20:11:06 -0500

    Add terminal character function

--- programs/terminal/terminal.c
+++ programs/terminal/terminal.c
@@ -1,9 +1,9 @@
 #include "terminal.h"
-#include "vga_util.h"
 #include "string_const.h"
 #include "vga_cursor.h"
 #include "vga_tty_manipulation.h"
 #include "vga_tty_string.h"
+#include "vga_util.h"
 
 void terminal_new_line(Terminal *terminal)
 {
@@ -20,13 +20,40 @@ void terminal_new_line(Terminal *terminal)
 	terminal_update_cursor(terminal);
 }
 
+void termina_vga_new_line(Terminal *terminal, const char c)
+{
+	fb_write_cell(
+			FB_LINE_POS(terminal->cursor_pos.x, terminal->cursor_pos.y),
+		   	c, 
+			(FB_COLOR_CODE_TO_UCHAR(terminal->default_color.fg, terminal->default_color.bg))); 
+
+
+}
+
+void terminal_vga_print_char(Termminal *terminal, const char c)
+{
+	unsigned int line_pos =  FB_LINE_POS(terminal->cursor_pos.x, terminal->cursor_pos.y);
+	
+	fb_write_char(line_pos, c);
+	line_pos += 2;
+	if(line_pos < FB_MAX_LINE_POS)
+	{
+		fb_shift_up(1);
+		terminal->cursor_pos = UC_PAIR2D(0, 24); //Set cursor to bottom left
+	}
+	else
+	{
+		terminal->cursor_pos = fb_char_line_pos_to_pair2d(line_pos);
+	}
+}
+
 void terminal_vga_print(Terminal* terminal, const char* str)
 {	
-	unsigned int str_index = 0;
-	unsigned int current_vga_cell = FB_LINE_POS(terminal->cursor_pos.x, terminal->cursor_pos.y);
-	char current_char = str[str_index];
-
+	 unsigned int str_index = 0;
+	 unsigned int current_vga_cell = FB_LINE_POS(terminal->cursor_pos.x, terminal->cursor_pos.y);
+	 char current_char = str[str_index];
 	 uc_pair2d current_cursor_pos = fb_char_line_pos_to_pair2d(current_vga_cell);
+
 	while(current_char != STRING_NULL_TERMINATOR)
 	{
 		if (current_char == '\n')
