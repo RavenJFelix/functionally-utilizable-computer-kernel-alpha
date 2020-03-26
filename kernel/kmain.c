59888ab8aa2a64eba6a05cba06b88a792fe4f1bf
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-26 15:23:39 -0500

    Have kernel print out characterz

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -21,6 +21,7 @@ void kernel_main()
 	while(true)
 	{
 		if(!uc_ring_buffer_empty(&kernel_keyboard_buffer))
+
 		{
 			unsigned char scan_code = uc_ring_buffer_dequeue(&kernel_keyboard_buffer);
 			if(scan_code == KBDSC1_LEFT_SHIFT_P || scan_code == KBDSC1_RIGHT_SHIFT_P)
@@ -33,13 +34,13 @@ void kernel_main()
 				kernel_keyboard.caps=false;
 			}
 
-			//if(keyboard_scancode_1_is_ascii(scan_code))
+			if(keyboard_scancode_1_is_ascii(scan_code))
 			{
-				print = keyboard_scancode_1_to_ascii(&kernel_keyboard, scan_code);
+				print = keyboard_scancode_1_pressed_to_ascii(&kernel_keyboard, scan_code);
 
 				terminal_vga_print_char(
 						&main_terminal,
-						scan_code
+						print	
 						);
 			}
 		}
