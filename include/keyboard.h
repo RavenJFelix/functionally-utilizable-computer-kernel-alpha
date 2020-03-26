041f6f6527dc5d540fff0e01b2c173d860a0fd9c
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-25 23:14:02 -0500

    Add keyboard struct

--- include/keyboard.h
+++ include/keyboard.h
@@ -1,7 +1,12 @@
 #ifndef KEYBOAORD_STUFF_H
 #define KEYBOAORD_STUFF_H
+#include "bool.h"
 #include "io.h"
 #define KEYBOARD_DATA_PORT 0x60
+typedef struct
+{
+	bool caps;
+}Keyboard;
 
 unsigned char keyboard_read_scan_code();
 
