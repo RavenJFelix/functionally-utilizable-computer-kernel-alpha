3e4ead72ef7a53e666b98e6dab521e059221bd9f
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-25 23:37:43 -0500

    Add functions to header

--- include/keyboard_ascii_conversion.h
+++ include/keyboard_ascii_conversion.h
@@ -1,6 +1,10 @@
 #ifndef KEYBOARD_ASCII_CONVERSION_RANDOMNESS_STUFF
 #define KEYBOARD_ASCII_CONVERSION_RANDOMNESS_STUFF
+#include "bool.h"
+#include "keyboard.h"
 char keyboard_scancode_1_to_ascii_number(unsigned char scan_code);
 char keyboard_scancode_1_to_ascii_letter_uppercase(unsigned char scan_code);
 char keyboard_scancode_1_to_ascii_letter_lowercase(unsigned char scan_code);
+char keyboard_scancode_1_to_ascii_letter(unsigned char scan_code, bool caps);
+char keyboard_scancode_1_to_ascii(Keyboard *keyboard, unsigned char scan_code);
 #endif
