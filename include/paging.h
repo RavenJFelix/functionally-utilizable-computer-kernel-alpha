0e037078d3b0096a94ecaefec3150578760eb63c
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-27 23:44:11 -0500

    add paging functions to header

--- include/paging.h
+++ include/paging.h
@@ -3,4 +3,8 @@
 #define PAGE_FREE 0x01
 #define PAGE_USED 0x00
 typedef void* pageframe_t;
+
+pageframe_t kcontinuous_alloc(unsigned long number_of_continous_frames);
+void kfree_frame(pageframe_t page_address);
+
 #endif
