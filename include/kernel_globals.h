b466fc105d67d144963937fa45652dd6376c7727
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-30 08:17:09 -0500

    Add actual address parameters

--- include/kernel_globals.h
+++ include/kernel_globals.h
@@ -7,7 +7,7 @@
 #include "ring_buffer.h"
 #define KERNEL_KEYBOARD_BUFFER_SIZE 256
 #define FIRST_FRAME_OFFSET 0x1000
-#define FIRST_FRAME_ADDRESS_UNALIGNED ((_virtual_kernel_end + FIRST_FRAME_OFFSET) % 0x1000)
+#define FIRST_FRAME_ADDRESS_UNALIGNED ((_physical_kernel_end + FIRST_FRAME_OFFSET) % 0x1000)
 #define FIRST_FRAME_ADDRESS (void*) (FIRST_FRAME_ADDRESS_UNALIGNED - (FIRST_FRAME_ADDRESS_UNALIGNED % 0x1000))
 void kernel_globals_init();
 extern Terminal main_terminal;
@@ -18,8 +18,12 @@ extern unsigned long kernel_page_directory[1024];
 //262144] is 1 GiB
 //Frame map is 2 GiB large for 4 KiB large pages
 extern unsigned char kernel_frame_map[KERNEL_DYNAMIC_MEMORY_PAGES];
-extern unsigned long _physical_kernel_start;
-extern unsigned long _virtual_kernel_start;
-extern unsigned long _physical_kernel_end;
-extern unsigned long _virtual_kernel_end;
+extern unsigned int _physical_kernel_start;
+extern unsigned int _virtual_kernel_start;
+extern unsigned int _physical_kernel_end;
+extern unsigned int _virtual_kernel_end;
+#define PHYSICAL_KERNEL_START_ADDRESS ((unsigned long) (&_physical_kernel_start))
+#define PHYSICAL_KERNEL_END_ADDRESS ((unsigned long) (&_physical_kernel_end))
+#define VIRTUAL_KERNEL_START_ADDRESS ((unsigned long) (&_virtual_kernel_start))
+#define VIRTUAL_KERNEL_END_ADDRESS ((unsigned long) (&_virtual_kernel_end))
 #endif
